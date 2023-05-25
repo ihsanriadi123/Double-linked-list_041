ac36f4f61460a45b05403d80db4772da4a1f3a5f
ihsan putra riadi <iputrariadi@gmail.com>
2023-05-25 10:29:45 +0700

    Menambahkan definisi kelas DOUBLELINKEDLIST

--- Double linked list_041/Double linked list_041.cpp
+++ Double linked list_041/Double linked list_041.cpp
@@ -10,3 +10,19 @@ public:
 	node* prev;
 };
 
+class DOUBLELINKEDLIST {
+private:
+	node* start;
+public:
+
+	DOUBLELINKEDLIST();
+	void addnode();
+	bool search(int rollNo, node** previous, node** current);
+	bool deletenode(int  rollNo);
+	bool listempty();
+	void ascending();
+	void descending();
+	void hapus();
+	void searchdata();
+};
+
