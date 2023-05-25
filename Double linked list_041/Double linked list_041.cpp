eaf631a9124112d2d3a523ea5ff0e6fb99b995ca
ihsan putra riadi <iputrariadi@gmail.com>
2023-05-25 11:00:58 +0700

    "Menambahkan logika penambahan node baru pada posisi yang tepat dalam linked list"

--- Double linked list_041/Double linked list_041.cpp
+++ Double linked list_041/Double linked list_041.cpp
@@ -55,5 +55,14 @@ void DOUBLELINKEDLIST::addnode() {
 		return;
 	}
 
+	node* current = start;
+	while (current->next != NULL && nim > current->next->noMhs)
+		current = current->next;
+
+	if (current->next != NULL && nim == current->next->noMhs) {
+		cout << "\nDuplicate roll numbers not allowed" << endl;
+		return;
+	}
+
 
 }
